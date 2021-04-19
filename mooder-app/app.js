98d4ce9bdba5acb301529e8aa01a5d0679148c96
MadalinaAnghel <madalina.anghel948@yahoo.com>
2021-04-19 22:35:16 +0300

    prepare for deployment

--- mooder-app/app.js
+++ mooder-app/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 express = require("express");
+const path = require("path");
 const session = require("express-session");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -12,13 +13,14 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.use('/uploads', express.static('uploads')); 
+app.use('/uploads', express.static('uploads'));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(session({ secret: process.env.SESSION_SECRET, cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/mooderDB", {
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SERVER}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -30,6 +32,11 @@ require("./config/passport");
 
 //routes
 var usersRouter = require("./routes/users");
-app.use("/users", usersRouter);
+app.use("/api/users", usersRouter);
 
-app.listen(9000, () => console.log("Server running on http://localhost:9000/"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on ${port}.`));
